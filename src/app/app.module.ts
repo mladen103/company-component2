1ce481b69665d8541878a4ace39e7608465c443b
mladja97 <djomla544@gmail.com>
2019-06-26 10:15:44 +0200

    template division

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,54 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './shared/components/layout/layout.component';
 
+const routes : Routes = [
+  {
+    path : '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './home/home.module#HomeModule'  
+      }
+    ]
+  },
 
+  {
+    path : 'about-us',
+    redirectTo: 'about-us',
+    pathMatch: 'full'
+  },
+  {
+    path: 'about-us',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './about-us/about-us.module#AboutUsModule'  
+      }
+    ]
+    //loadChildren: './about-us/about-us.module#AboutUsModule'
+  }
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/components/layout/layout.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { 
+
+      // Zbog template-a i eksternih js-ova, nije se naslo bolje resenje..
+
+      document.getElementById("modernizrScript").remove();
+      var modernizrScript = document.createElement("script");
+      modernizrScript.setAttribute("id", "modernizrScript");
+      modernizrScript.setAttribute("src", "assets/js/modernizr-2.6.2.min.js");
+      document.body.appendChild(modernizrScript);
+  
+      document.getElementById("jqueryScript").remove();
+      var jqueryScript = document.createElement("script");
+      jqueryScript.setAttribute("id", "jqueryScript");
+      jqueryScript.setAttribute("src", "assets/jquery/jquery.min.js");
+      document.body.appendChild(jqueryScript);
+  
+      document.getElementById("jqueryEasingScript").remove();
+      var script2 = document.createElement("script");
+      script2.setAttribute("id", "jqueryEasingScript");
+      script2.setAttribute("src", "assets/js/jquery.easing.1.3.js");
+      document.body.appendChild(script2);
+
+      document.getElementById("bootstrapBundleScript").remove();
+      var bootstrapBundleScript = document.createElement("script");
+      bootstrapBundleScript.setAttribute("id", "bootstrapBundleScript");
+      bootstrapBundleScript.setAttribute("src", "node_modules/bootstrap/js/bootstrap.bundle.min.js");
+      document.body.appendChild(bootstrapBundleScript);
+  
+      document.getElementById("jqueryWaypointsScript").remove();
+      let jqueryWaypointsScript = document.createElement("script");
+      jqueryWaypointsScript.setAttribute("id", "jqueryWaypointsScript");
+      jqueryWaypointsScript.setAttribute("src", "assets/js/jquery.waypoints.min.js");
+      document.body.appendChild(jqueryWaypointsScript);
+  
+      document.getElementById("flexSliderScript").remove();
+      let flexSliderScript = document.createElement("script");
+      flexSliderScript.setAttribute("id", "flexSliderScript");
+      flexSliderScript.setAttribute("src", "assets/js/jquery.flexslider-min.js");
+      document.body.appendChild(flexSliderScript);
+  
+      document.getElementById("mainScript").remove();
+      let mainScript = document.createElement("script");
+      mainScript.setAttribute("id", "mainScript");
+      mainScript.setAttribute("src", "assets/js/main.js");
+      document.body.appendChild(mainScript);
+  
+      document.getElementById("testScript").remove();
+      let testScript = document.createElement("script");
+      testScript.setAttribute("id", "testScript");
+      testScript.setAttribute("src", "assets/js/test.js");
+      document.body.appendChild(testScript);
+
+  }
+
+  ngOnInit() {
+  }
+
+}
